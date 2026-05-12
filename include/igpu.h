0c33eb10765daa144dec18c0a9aa40b613f961ca
nwmarino <nwmarinoto@gmail.com>
2026-05-11 21:23:48 -0400

    Add initial files, CMake setup

---
+++ include/igpu.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2026 Nick Marino
+//  All rights reserved.
+//
+
+#ifndef IGPU_H_
+#define IGPU_H_
+
+namespace gpu {
+
+
+    
+} // namespace gpu
+
+#endif // IGPU_H_
---
+++ source/igpu.cpp
@@ -0,0 +1,5 @@
+//
+//  Copyright (c) 2026 Nick Marino
+//  All rights reserved.
+//
+
